931c72a42aa05107409a4a69fe69f693a6ae8878
ugwu thaddeus <thaddeusugwu2007@gmail.com>
2025-10-11 19:48:54 +0100

    updates

--- flash-pay/js/bankDetails.js
+++ flash-pay/js/bankDetails.js
@@ -1,37 +1,196 @@
+// window.addEventListener("DOMContentLoaded", async () => {
+//   initLogoutButton();
+// const userAccountName = document.getElementById('useraccountName')
+// const userAccountNumber = document.getElementById("useraccountNumber")
+// const userBankName = document.getElementById('userbankName')
+// function loadBankDetails() { if(userAccountName && userAccountNumber && userBankName){
+//   try{
+//     const response = await fetch("https://prime-invest-server.onrender.com/api/users/bank-details",{
+//       method:"GET",
+//       headers:{"content-type":"application/json"},
+//       credentials:"include"
+//     })
+
+//     data = await response.json();
+
+//     if (response.ok && data.success){
+//       const detail = data.data;
+//       userAccountName.textContent = detail.accountName;
+//       userAccountNumber.textContent = detail.accountNumber;
+//       userBankName.textContent = detail.bankName
+//       if (detail.bankName){document.getElementById("bankDetailsDiv").classList.remove("hidden");};
+//       if(!detail.bankName){
+//         document.getElementById("bankDetailsDiv").classList.add("hidden");
+//         document.getElementById("bankDetails").classList.remove("hidden");
+//       }
+//     }else{
+//       document.getElementById("bankDetailsDiv").classList.remove("hidden");
+//       document.getElementById("bankDetails").classList.add("hidden");
+      
+//     }
+//   }catch (error) {
+//     console.error("Error fetching user:", error);
+//   };}}
+//   document.getElementById('editDetailsBtn').addEventListener("click",()=>{
+//     document.getElementById("bankDetails").classList.remove("hidden");
+//   });
+
+//   document
+//     .getElementById("bankDetailsForm")
+//     .addEventListener("submit", async function (e) {
+//       e.preventDefault();
+
+//       const bankName = document.getElementById("bankName").value.trim();
+//       const accountNumber = document
+//         .getElementById("accountNumber")
+//         .value.trim();
+//       const accountName = document.getElementById("accountName").value.trim();
+
+//       let hasError = false;
+//       if (!bankName || !accountName || !accountNumber) {
+//         document.getElementById("bankPageError").classList.remove("hidden");
+//         hasError = true;
+//       } else {
+//         document.getElementById("bankPageError").classList.add("hidden");
+//       }
+
+//       if (hasError) return;
+//       document.getElementById("sendBtn").textContent = "submitting";
+
+//       try {
+//         const response = await fetch(
+//           "https://prime-invest-server.onrender.com/api/users/bank",
+//           {
+//             method: "PATCH",
+//             headers: {
+//               "Content-Type": "application/json",
+//             },
+//             credentials: "include",
+//             body: JSON.stringify({
+//               bankName,
+//               accountNumber,
+//               accountName,
+//             }),
+//           }
+//         );
+
+//         let data;
+//         try {
+//           data = await response.json();
+//         } catch {
+//           throw new Error("Invalid JSON response from server");
+//         }
+
+//         if (response.ok && data.success) {
+//           loadBankDetails();
+//           alert("Bank details submitted successfully ✅");
+//           document.getElementById("bankDetailsForm").reset(); 
+//           document.getElementById("bankDetails").classList.add("hidden");
+//            document.getElementById("sendBtn").textContent = "send";
+//         } else {
+//           alert(data.message || "An error occurred ❌");
+//            document.getElementById("sendBtn").textContent = "send";
+//         }
+//       } catch (err) {
+//         console.error(err);
+//         alert("Something went wrong. Please try again later.");
+//          document.getElementById("sendBtn").textContent = "send";
+//       }
+//     });
+// });
+// function initLogoutButton() {
+//   const logoutBtn = document.getElementById("logoutBtn");
+//   if (!logoutBtn) return; 
+
+//   logoutBtn.addEventListener("click", async () => {
+//     const originalHTML = logoutBtn.innerHTML;
+
+//     logoutBtn.disabled = true;
+//     logoutBtn.innerHTML = `<i class="fas fa-spinner fa-spin mr-2"></i>Logging out...`;
+
+//     try {
+//       const res = await fetch("https://prime-invest-server.onrender.com/api/auth/logout", {
+//         method: "POST",
+//         credentials: "include", 
+//         headers: {
+//           "Content-Type": "application/json",
+//         },
+//       });
+
+//       const data = await res.json();
+
+//       if (res.ok) {
+//         alert('logged out successfully')
+//         // Clear storage
+//         localStorage.clear();
+//         sessionStorage.clear();
+
+    
+//         setTimeout(() => {
+//           window.location.href = "./login.html";
+//         }, 1500);
+//       } else {
+//         logoutBtn.disabled = false;
+//         alert(data.message)
+//         logoutBtn.innerHTML = originalHTML;
+//       }
+//     } catch (err) {
+//       console.error("Logout error:", err);
+//       alert(err|| data.message)
+//       logoutBtn.disabled = false;
+//       logoutBtn.innerHTML = originalHTML;
+//     }
+//   });
+// }
 window.addEventListener("DOMContentLoaded", async () => {
   initLogoutButton();
-const userAccountName = document.getElementById('useraccountName')
-const userAccountNumber = document.getElementById("useraccountNumber")
-const userBankName = document.getElementById('userbankName')
-if(userAccountName && userAccountNumber && userBankName){
-  try{
-    const response = await fetch("https://prime-invest-server.onrender.com/api/users/bank-details",{
-      method:"GET",
-      headers:{"content-type":"application/json"},
-      credentials:"include"
-    })
 
-    data = await response.json();
+  const userAccountName = document.getElementById("useraccountName");
+  const userAccountNumber = document.getElementById("useraccountNumber");
+  const userBankName = document.getElementById("userbankName");
 
-    if (response.ok && data.success){
-      const detail = data.data;
-      userAccountName.textContent = detail.accountName;
-      userAccountNumber.textContent = detail.accountNumber;
-      userBankName.textContent = detail.bankName
-      if (detail.bankName){document.getElementById("bankDetailsDiv").classList.remove("hidden");};
-      if(!detail.bankName){
-        document.getElementById("bankDetailsDiv").classList.add("hidden");
-        document.getElementById("bankDetails").classList.remove("hidden");
+  // ✅ make this function async
+  async function loadBankDetails() {
+    if (userAccountName && userAccountNumber && userBankName) {
+      try {
+        const response = await fetch(
+          "https://prime-invest-server.onrender.com/api/users/bank-details",
+          {
+            method: "GET",
+            headers: { "Content-Type": "application/json" },
+            credentials: "include",
+          }
+        );
+
+        const data = await response.json();
+
+        if (response.ok && data.success) {
+          const detail = data.data;
+          userAccountName.textContent = detail.accountName || "";
+          userAccountNumber.textContent = detail.accountNumber || "";
+          userBankName.textContent = detail.bankName || "";
+
+          if (detail.bankName) {
+            document.getElementById("bankDetailsDiv").classList.remove("hidden");
+            document.getElementById("bankDetails").classList.add("hidden");
+          } else {
+            document.getElementById("bankDetailsDiv").classList.add("hidden");
+            document.getElementById("bankDetails").classList.remove("hidden");
+          }
+        } else {
+          document.getElementById("bankDetailsDiv").classList.remove("hidden");
+          document.getElementById("bankDetails").classList.add("hidden");
+        }
+      } catch (error) {
+        console.error("Error fetching user:", error);
       }
-    }else{
-      document.getElementById("bankDetailsDiv").classList.remove("hidden");
-      document.getElementById("bankDetails").classList.add("hidden");
-      
     }
-  }catch (error) {
-    console.error("Error fetching user:", error);
-  };}
-  document.getElementById('editDetailsBtn').addEventListener("click",()=>{
+  }
+
+  // ✅ call the function to load data initially
+  await loadBankDetails();
+
+  document.getElementById("editDetailsBtn").addEventListener("click", () => {
     document.getElementById("bankDetails").classList.remove("hidden");
   });
 
@@ -41,9 +200,7 @@ if(userAccountName && userAccountNumber && userBankName){
       e.preventDefault();
 
       const bankName = document.getElementById("bankName").value.trim();
-      const accountNumber = document
-        .getElementById("accountNumber")
-        .value.trim();
+      const accountNumber = document.getElementById("accountNumber").value.trim();
       const accountName = document.getElementById("accountName").value.trim();
 
       let hasError = false;
@@ -55,16 +212,16 @@ if(userAccountName && userAccountNumber && userBankName){
       }
 
       if (hasError) return;
-      document.getElementById("sendBtn").textContent = "submitting";
+
+      const sendBtn = document.getElementById("sendBtn");
+      sendBtn.textContent = "Submitting...";
 
       try {
         const response = await fetch(
           "https://prime-invest-server.onrender.com/api/users/bank",
           {
             method: "PATCH",
-            headers: {
-              "Content-Type": "application/json",
-            },
+            headers: { "Content-Type": "application/json" },
             credentials: "include",
             body: JSON.stringify({
               bankName,
@@ -74,68 +231,59 @@ if(userAccountName && userAccountNumber && userBankName){
           }
         );
 
-        let data;
-        try {
-          data = await response.json();
-        } catch {
-          throw new Error("Invalid JSON response from server");
-        }
+        const data = await response.json();
 
         if (response.ok && data.success) {
+          await loadBankDetails();
           alert("Bank details submitted successfully ✅");
-          document.getElementById("bankDetailsForm").reset(); 
+          document.getElementById("bankDetailsForm").reset();
           document.getElementById("bankDetails").classList.add("hidden");
-           document.getElementById("sendBtn").textContent = "send";
         } else {
           alert(data.message || "An error occurred ❌");
-           document.getElementById("sendBtn").textContent = "send";
         }
       } catch (err) {
         console.error(err);
         alert("Something went wrong. Please try again later.");
-         document.getElementById("sendBtn").textContent = "send";
+      } finally {
+        sendBtn.textContent = "Send";
       }
     });
 });
+
 function initLogoutButton() {
   const logoutBtn = document.getElementById("logoutBtn");
-  if (!logoutBtn) return; 
+  if (!logoutBtn) return;
 
   logoutBtn.addEventListener("click", async () => {
     const originalHTML = logoutBtn.innerHTML;
-
     logoutBtn.disabled = true;
-    logoutBtn.innerHTML = `<i class="fas fa-spinner fa-spin mr-2"></i>Logging out...`;
+    logoutBtn.innerHTML = `<i class="fas fa-spinner fa-spin mr-2"></i> Logging out...`;
 
     try {
       const res = await fetch("https://prime-invest-server.onrender.com/api/auth/logout", {
         method: "POST",
-        credentials: "include", 
-        headers: {
-          "Content-Type": "application/json",
-        },
+        credentials: "include",
+        headers: { "Content-Type": "application/json" },
       });
 
       const data = await res.json();
 
       if (res.ok) {
-        alert('logged out successfully')
-        // Clear storage
+        alert("Logged out successfully ✅");
         localStorage.clear();
         sessionStorage.clear();
 
-    
         setTimeout(() => {
           window.location.href = "./login.html";
         }, 1500);
       } else {
+        alert(data.message || "Logout failed");
         logoutBtn.disabled = false;
-        alert(data.message)
         logoutBtn.innerHTML = originalHTML;
       }
     } catch (err) {
       console.error("Logout error:", err);
-      alert(err|| data.message)
+      alert("Something went wrong during logout ❌");
       logoutBtn.disabled = false;
       logoutBtn.innerHTML = originalHTML;
     }
